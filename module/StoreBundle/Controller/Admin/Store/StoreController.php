3d52459940175425513bb873ff13443a79b9d8d0
Daan Wendelen <daanwendelen@gmail.com>
2014-01-19 11:03:12 +0100

    creating store object, with babysteps

---
+++ module/StoreBundle/Controller/Admin/Store/StoreController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Daan Wendelen <daanwendelen@gmail.com>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace StoreBundle\Controller\Admin\Store;
+
+use StoreBundle\Entity\Store\Store,
+    Zend\View\Model\ViewModel;
+
+/**
+ * ArticleController
+ *
+ * @author Daan Wendelen <daanwendelen@gmail.com>
+ */
+class StoreController extends \CudiBundle\Component\Controller\ActionController
+{
+    public function createAction()
+    {
+        
+    }
+}
---
+++ module/StoreBundle/Entity/Store/Store.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Daan Wendelen <daanwendelen@gmail.com>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace StoreBundle\Entity\Store;
+
+/**
+ * @ORM\Entity(repositoryClass="StoreBundle\Repository\Store\Store")
+ * @ORM\Table(name="store.store")
+ * @ORM\InheritanceType("JOINED")
+ * @ORM\DiscriminatorColumn(name="inheritance_type", type="string")
+ *
+ * @author Daan Wendelen <daanwendelen@gmail.com>
+ */
+abstract class Store
+{
+    
+    
+    public function __construct()
+    {
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @var integer The ID of this article
+     *
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="bigint")
+     */
+    private $id;
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Factory Only
+     *
+     * @param string $name
+     *
+     * @return \StoreBundle\Entity\Store\Store
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @var string The name of this store
+     *
+     * @ORM\Column(type="string")
+     */
+    private $name;
+}
