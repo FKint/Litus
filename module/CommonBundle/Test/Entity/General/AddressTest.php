9f61faa7b7c98ad8734186b0559263614458c6ab
Daan Wendelen <daanwendelen@gmail.com>
2014-09-07 01:08:22 +0200

    Test Addresses

---
+++ module/CommonBundle/Test/Entity/General/AddressTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Litus is a project by a group of students from the KU Leuven. The goal is to create
+ * various applications to support the IT needs of student unions.
+ *
+ * @author Niels Avonds <niels.avonds@litus.cc>
+ * @author Karsten Daemen <karsten.daemen@litus.cc>
+ * @author Koen Certyn <koen.certyn@litus.cc>
+ * @author Bram Gotink <bram.gotink@litus.cc>
+ * @author Dario Incalza <dario.incalza@litus.cc>
+ * @author Pieter Maene <pieter.maene@litus.cc>
+ * @author Kristof Mariën <kristof.marien@litus.cc>
+ * @author Lars Vierbergen <lars.vierbergen@litus.cc>
+ * @author Daan Wendelen <daan.wendelen@litus.cc>
+ *
+ * @license http://litus.cc/LICENSE
+ */
+
+namespace CommonBundle\Test\Entity\General;
+
+use CommonBundle\Entity\General\Address\City;
+use CommonBundle\Entity\General\Address\Street;
+class AddressTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCity()
+    {
+        $city = new City(666, 'The city of the beast');
+        $street1 = new Street($city, 1, 'Ett');
+        $street2 = new Street($city, 2, 'Två');
+        $street3 = new Street($city, 3, 'Tre');
+        $street4 = new Street($city, 4, 'Fyra');
+        
+        $this->assertEquals('Ett', $street1->getName());
+        //$this->assertEquals(1, $street1->getId());
+        //$this->assertEquals($city, $street1->getCity());
+        
+        $this->assertEquals('The city of the beast', $city->getName());
+        $this->assertEquals(666, $city->getPostal());
+        $this->assertEquals(4, count($city->getStreets()));
+        $this->assertContains($street1, $city);
+        $this->assertContains($street2, $city);
+        $this->assertContains($street3, $city);
+        $this->assertContains($street4, $city);
+    }
+}
